d4e0ea42d7ae1b52b5c7fb75576722f0b419b38c
Mariola Bartosik <56107837+mariolciax@users.noreply.github.com>
2022-05-11 10:03:01 +0200

    Add files via upload

--- lab_2_infa.R
+++ lab_2_infa.R
@@ -36,6 +36,7 @@ przedzial(waga,0.99)
 przedzial.sred<-function(dane,poziom.ufnosci){
   if(length(dane)<100) {print("BLAD za ma³o obserwacji")}
   else{
+    na.omit(dane)
     alfa=1-poziom.ufnosci
     d<-qnorm(1-alfa/2)*sd(dane)/sqrt(length(dane))
     mean(dane)+c(-1,1)*d
@@ -47,3 +48,15 @@ przedzial.sred(waga,0.99)
 
 #4b
 library(MASS)
+duration<-geyser$duration
+przedzial.sred(duration,0.95)
+
+przedzial(duration,0.95)
+
+
+#6 MODEL 1
+
+#7
+binom.test(21,25,conf.level = 0.9)$conf.int
+
+?prop.test()
